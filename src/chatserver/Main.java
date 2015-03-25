63c093dfc7435e428ece4aa44024af86e60707d9
Daryl Johnston <daryl874@gmail.com>
2015-03-25 18:38:51 +0000

    refactor

--- src/chatserver/Main.java
+++ src/chatserver/Main.java
@@ -15,13 +15,9 @@ public class Main {
         BufferedReader stdIn  = new BufferedReader(
                                 new InputStreamReader(System.in));
         byte[]          buf = null;
-       // ServerSocket    serverSocket = new ServerSocket(4445);
-        //Socket          sock = null;
-        int             ticket = 0;
         DatagramSocket    socket = new DatagramSocket(4445);
         DatagramPacket[] packets = new DatagramPacket[100];
         ChatThread[] threads = new ChatThread[100];
-        String userName;
         
         int numClients = 0;
 
@@ -36,53 +32,9 @@ public class Main {
 
             threads[numClients] = new ChatThread(packets[numClients], socket);
             threads[numClients].start();
-           // System.out.println("Packet Recieved- Port: " + port);
 
             
             
         }
     }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*
-        BufferedReader stdIn  = new BufferedReader(
-                                new InputStreamReader(System.in));
-        byte[] buf = null;
-        DatagramSocket    socket = new DatagramSocket(4445);
-        DatagramPacket    packet = null;
-
-        while (true) {
-            try {                
-                buf = new byte[256];
-                packet = new DatagramPacket(buf, buf.length);
-                socket.receive(packet);         // receive request
-
-                String request = new String(packet.getData());
-                System.out.println("ChatServer: Request from client: " + request);
-                System.out.println("ChatServer: Type in your response:");
-                
-                String serverResponse = stdIn.readLine();
-                buf = serverResponse.getBytes();
-
-                InetAddress address = packet.getAddress();
-                int         port    = packet.getPort();
-
-                packet = new DatagramPacket(buf, buf.length, address, port);
-
-                String response = new String(packet.getData());
-                System.out.println("ChatServer: Sending response:" + response);
-                socket.send(packet);
-    
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
-    }
+}
